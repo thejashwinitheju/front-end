ae8dc8569add92dbdfbe50706af1fac45cd8400a
thejashwinitheju <thejashwinitheju912@gmail.com>
2022-11-05 17:14:48 +0530

    nutirition

---
+++ javascript/object.js
@@ -0,0 +1,18 @@
+
+/*console.log("hello")
+
+
+
+function number(){
+console.log("theju")
+}
+number()
+var name="theju";
+var emp={id:101, sal:10000};
+console.log(emp);*/
+var salary=10000;
+if(salary>1000){
+    console.log("more " )
+}else{
+    console.log("less")
+}
