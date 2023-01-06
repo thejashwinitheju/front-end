2a6f6c61d50d6c30cafc0ef67010fc0f05d12971
thejashwinitheju <thejashwinitheju912@gmail.com>
2023-01-06 10:42:56 +0530

    object

--- javascript/object.js
+++ javascript/object.js
@@ -1,18 +1,34 @@
 
-/*console.log("hello")
+// /*console.log("hello")
 
 
 
-function number(){
-console.log("theju")
-}
-number()
-var name="theju";
-var emp={id:101, sal:10000};
-console.log(emp);*/
-var salary=10000;
-if(salary>1000){
-    console.log("more " )
-}else{
-    console.log("less")
-}
+// function number(){
+// console.log("theju")
+// }
+// number()
+// var name="theju";
+// var emp={id:101, sal:10000};
+// console.log(emp);*/
+// var salary=10000;
+// if(salary>1000){
+//     console.log("more " )
+// }else{
+//     console.log("less")
+// }
+
+
+// Setup
+// const myArray = [["John", 23], ["dog", 3]];
+
+// // Only change code below this line
+// const removedFromMYArray = myArray.shift();
+// console.log(removedFromMYArray);
+
+function timesFive(num){
+    return num * 5;
+    
+    
+    } 
+    const answer = timesFive(1);
+    console.log(answer);
